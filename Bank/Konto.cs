24edbc69b2ffbe2926410b823bd0aa2766f89a6d
Michał Wojciechowski <michal.wojciechowsk3@microsoft.wsei.edu.pl>
2025-04-02 18:27:59 +0200

    Dodaj pliki projektów.

---
+++ Bank/Konto.cs
@@ -0,0 +1,59 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        private string klient;
+        private decimal bilans;
+        private bool zablokowane = false;
+
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+
+        public string Nazwa => klient;
+        public decimal Bilans => bilans;
+        public bool Zablokowane => zablokowane;
+
+        public void Wplata(decimal kwota)
+        {
+            if (zablokowane)
+            {
+                throw new InvalidOperationException("Konto jest zablokowane. Nie można dokonać wpłaty.");
+            }
+            if (kwota <= 0)
+            {
+                throw new ArgumentException("Kwota wpłaty musi być większa od zera.");
+            }
+            bilans += kwota;
+        }
+
+        public void Wyplata(decimal kwota)
+        {
+            if (zablokowane)
+            {
+                throw new InvalidOperationException("Konto jest zablokowane. Nie można dokonać wypłaty.");
+            }
+            if (kwota <= 0)
+            {
+                throw new ArgumentException("Kwota wypłaty musi być większa od zera.");
+            }
+            if (kwota > bilans)
+            {
+                throw new InvalidOperationException("Niewystarczające środki na koncie.");
+            }
+            bilans -= kwota;
+        }
+
+        public void BlokujKonto()
+        {
+            zablokowane = true;
+        }
+
+        public void OdblokujKonto()
+        {
+            zablokowane = false;
+        }
+    }
+}
---
+++ TestBank/Test1.cs
@@ -0,0 +1,84 @@
+﻿using Bank;
+
+namespace TestBank
+{
+    [TestClass]
+    public class TestBank
+    {
+        private Konto konto;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            konto = new Konto("Jan Kowalski", 100);
+        }
+
+        [TestMethod]
+        public void Wplata_PrawidłowaKwota_ZwiększaBilans()
+        {
+            konto.Wplata(50);
+            Assert.AreEqual(150, konto.Bilans);
+        }
+
+        [TestMethod]
+        public void Wplata_NegatywnaKwota_ThrowsArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => konto.Wplata(-50));
+        }
+
+        [TestMethod]
+        public void Wplata_KontoZablokowane_ThrowsInvalidOperationException()
+        {
+            konto.BlokujKonto();
+            Assert.ThrowsException<InvalidOperationException>(() => konto.Wplata(50));
+        }
+
+        [TestMethod]
+        public void Wyplata_PrawidłowaKwota_ZmniejszaBilans()
+        {
+            konto.Wyplata(50);
+            Assert.AreEqual(50, konto.Bilans);
+        }
+        [TestMethod]
+        public void Wyplata_NegatywnaKwota_ThrowsArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(-50));
+        }
+
+        [TestMethod]
+        public void Wyplata_NiewystarczająceSrodki_ThrowsInvalidOperationException()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => konto.Wyplata(150));
+        }
+
+        [TestMethod]
+        public void Wyplata_KontoZablokowane_ThrowsInvalidOperationException()
+        {
+            konto.BlokujKonto();
+            Assert.ThrowsException<InvalidOperationException>(() => konto.Wyplata(50));
+        }
+
+        [TestMethod]
+        public void BlokujKonto_UstawiaStanZablokowane()
+        {
+            konto.BlokujKonto();
+            Assert.IsTrue(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void OdblokujKonto_UstawiaStanNieZablokowane()
+        {
+            konto.BlokujKonto();
+            konto.OdblokujKonto();
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void Konstruktor_InicjalizujePola()
+        {
+            Assert.AreEqual("Jan Kowalski", konto.Nazwa);
+            Assert.AreEqual(100, konto.Bilans);
+            Assert.IsFalse(konto.Zablokowane);
+        }
+    }
+}
