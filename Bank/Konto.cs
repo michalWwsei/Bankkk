6bd48b7e4109aed9e3274f2ebf97733ad02339bb
Michał Wojciechowski <michal.wojciechowsk3@microsoft.wsei.edu.pl>
2025-04-04 00:17:52 +0200

    dalej

--- Bank/Konto.cs
+++ Bank/Konto.cs
@@ -3,7 +3,7 @@
     public class Konto
     {
         private string klient;
-        private decimal bilans;
+        protected decimal bilans;
         private bool zablokowane = false;
 
         public Konto(string klient, decimal bilansNaStart = 0)
@@ -18,15 +18,16 @@
 
         public void Wplata(decimal kwota)
         {
-            if (zablokowane)
-            {
-                throw new InvalidOperationException("Konto jest zablokowane. Nie można dokonać wpłaty.");
-            }
             if (kwota <= 0)
             {
                 throw new ArgumentException("Kwota wpłaty musi być większa od zera.");
             }
-            bilans += kwota;
+
+            if (!zablokowane) 
+            {
+                bilans += kwota;
+            }
+            
         }
 
         public void Wyplata(decimal kwota)
@@ -56,4 +57,78 @@
             zablokowane = false;
         }
     }
+
+    public class KontoPlus : Konto
+    {
+        private decimal limitDebetowy;
+        private bool debetWykorzystany;
+
+        public KontoPlus(string klient, decimal bilansNaStart = 0, decimal limitDebetowy = 0) : base(klient, bilansNaStart)
+        {
+            this.limitDebetowy = limitDebetowy;
+            this.debetWykorzystany = false;
+        }
+
+        public decimal LimitDebetowy
+        {
+            get => limitDebetowy;
+            set => limitDebetowy = value;
+        }
+
+        public new decimal Bilans => base.Bilans + (debetWykorzystany ? 0 : limitDebetowy); 
+
+
+        public new void Wplata(decimal kwota)
+        {
+            base.Wplata(kwota);
+
+            if (base.Bilans >= 0 && debetWykorzystany)
+            {
+                OdblokujKonto();
+                debetWykorzystany = false;
+            }
+        }
+
+
+        public new void Wyplata(decimal kwota)
+        {
+            if (kwota <= 0)
+            {
+                throw new ArgumentException("Kwota wypłaty musi być większa od zera.");
+            }
+
+            if (Zablokowane && debetWykorzystany)
+            {
+                throw new InvalidOperationException("Konto jest zablokowane z powodu wykorzystania limitu debetowego.");
+            }
+            else if (Zablokowane)
+            {
+                throw new InvalidOperationException("Konto jest zablokowane.");
+            }
+
+            if (kwota > base.Bilans + (debetWykorzystany ? 0 : limitDebetowy))
+            {
+                throw new InvalidOperationException("Niewystarczające środki na koncie, łącznie z limitem debetowym.");
+            }
+
+            if (kwota > base.Bilans)
+            {
+                debetWykorzystany = true;
+
+                decimal dostepneSrodki = base.Bilans; 
+
+                if (dostepneSrodki > 0)
+                { 
+                    base.Wyplata(dostepneSrodki); 
+                }
+
+                BlokujKonto(); 
+            }
+            else
+            {
+                base.Wyplata(kwota);
+            }
+        }
+    }
 }
+
--- TestBank/Test1.cs
+++ TestBank/Test1.cs
@@ -27,10 +27,12 @@ namespace TestBank
         }
 
         [TestMethod]
-        public void Wplata_KontoZablokowane_ThrowsInvalidOperationException()
+        public void Wplata_KontoZablokowane_NieZmieniaBilansu()
         {
             konto.BlokujKonto();
-            Assert.ThrowsException<InvalidOperationException>(() => konto.Wplata(50));
+            decimal bilansPrzed = konto.Bilans;
+            konto.Wplata(50);
+            Assert.AreEqual(bilansPrzed, konto.Bilans); 
         }
 
         [TestMethod]
@@ -80,5 +82,68 @@ namespace TestBank
             Assert.AreEqual(100, konto.Bilans);
             Assert.IsFalse(konto.Zablokowane);
         }
+    }
+
+    [TestClass]
+    public class TestKontoPlus
+    {
+        private KontoPlus konto;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            konto = new KontoPlus("Jan Kowalski", 100, 50);
+        }
+
+        [TestMethod]
+        public void Konstruktor_InicjalizujePola()
+        {
+            Assert.AreEqual("Jan Kowalski", konto.Nazwa);
+            Assert.AreEqual(150, konto.Bilans); 
+            Assert.IsFalse(konto.Zablokowane);
+            Assert.AreEqual(50, konto.LimitDebetowy);
+        }
+
+
+        [TestMethod]
+        public void Wyplata_WykorzystanieDebetu_BlokujeKonto()
+        {
+            konto.Wyplata(120); 
+            Assert.AreEqual(0, konto.Bilans);  
+            Assert.IsTrue(konto.Zablokowane);
+        }
+
+
+        [TestMethod]
+        public void Wyplata_PrzekroczenieDebetu_ZwracaWyjatek()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => konto.Wyplata(200));
+        }
+
+        [TestMethod]
+        public void Wplata_PoWykorzystaniuDebetu_OdblokowujeKonto()
+        {
+            konto.Wyplata(120);  
+            Assert.IsTrue(konto.Zablokowane);
+            konto.Wplata(20);   
+            Assert.IsFalse(konto.Zablokowane);
+            Assert.AreEqual(50, konto.Bilans); 
+        }
+
+        [TestMethod]
+        public void ZmianaLimitu_ZmieniaDostepneSrodki()
+        {
+            konto.LimitDebetowy = 100;
+            Assert.AreEqual(200, konto.Bilans);
+        }
+
+        [TestMethod]
+        public void Wyplata_CzescioweWykorzystanieDebetu()
+        {
+            konto.Wyplata(50);
+            Assert.AreEqual(100, konto.Bilans); 
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
     }
 }
