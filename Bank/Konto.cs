f051a3d872b1bdc087c14e0a1564452dd3890d84
Michał Wojciechowski <michal.wojciechowsk3@microsoft.wsei.edu.pl>
2025-04-04 02:14:10 +0200

    dobranoc

--- Bank/Konto.cs
+++ Bank/Konto.cs
@@ -23,11 +23,11 @@
                 throw new ArgumentException("Kwota wpłaty musi być większa od zera.");
             }
 
-            if (!zablokowane) 
+            if (!zablokowane)
             {
                 bilans += kwota;
             }
-            
+
         }
 
         public void Wyplata(decimal kwota)
@@ -75,7 +75,7 @@
             set => limitDebetowy = value;
         }
 
-        public new decimal Bilans => base.Bilans + (debetWykorzystany ? 0 : limitDebetowy); 
+        public new decimal Bilans => base.Bilans + (debetWykorzystany ? 0 : limitDebetowy);
 
 
         public new void Wplata(decimal kwota)
@@ -115,14 +115,14 @@
             {
                 debetWykorzystany = true;
 
-                decimal dostepneSrodki = base.Bilans; 
+                decimal dostepneSrodki = base.Bilans;
 
                 if (dostepneSrodki > 0)
-                { 
-                    base.Wyplata(dostepneSrodki); 
+                {
+                    base.Wyplata(dostepneSrodki);
                 }
 
-                BlokujKonto(); 
+                BlokujKonto();
             }
             else
             {
@@ -130,5 +130,87 @@
             }
         }
     }
+
+    public class KontoLimit
+    {
+        private Konto konto;
+        private decimal limit;
+        private bool debetWykorzystany;
+
+        public KontoLimit(string klient, decimal bilansNaStart = 0, decimal limit = 0)
+        {
+            this.konto = new Konto(klient, bilansNaStart);
+            this.limit = limit;
+            this.debetWykorzystany = false;
+        }
+
+        public string Nazwa => konto.Nazwa;
+        public decimal Bilans => konto.Bilans + (debetWykorzystany ? 0 : limit);
+        public bool Zablokowane => konto.Zablokowane;
+        public decimal Limit
+        {
+            get => limit;
+            set => limit = value;
+        }
+
+
+        public void Wplata(decimal kwota)
+        {
+            if (!konto.Zablokowane || debetWykorzystany)
+            {
+                konto.Wplata(kwota);
+            }
+
+
+            if (konto.Bilans >= 0 && debetWykorzystany)
+            {
+                konto.OdblokujKonto();
+                debetWykorzystany = false;
+            }
+        }
+
+        public void Wyplata(decimal kwota)
+        {
+            if (kwota <= 0)
+            {
+                throw new ArgumentException("Kwota wypłaty musi być większa od zera.");
+            }
+
+            if (Zablokowane && debetWykorzystany)
+            {
+                throw new InvalidOperationException("Konto jest zablokowane z powodu wykorzystania limitu debetowego.");
+            }
+            else if (Zablokowane)
+            {
+                throw new InvalidOperationException("Konto jest zablokowane.");
+            }
+
+            if (kwota > konto.Bilans + (debetWykorzystany ? 0 : limit))
+            {
+                throw new InvalidOperationException("Niewystarczające środki na koncie, łącznie z limitem debetowym.");
+            }
+
+            if (kwota > konto.Bilans)
+            {
+                decimal dostepneSrodki = konto.Bilans;
+
+                if (dostepneSrodki > 0)
+                {
+                    konto.Wyplata(dostepneSrodki); 
+                }
+
+                if (!debetWykorzystany) 
+                {
+                    debetWykorzystany = true;
+                    konto.BlokujKonto();
+                }
+            }
+            else
+            {
+                konto.Wyplata(kwota);
+            }
+        }
+    }
 }
+
 
--- TestBank/Test1.cs
+++ TestBank/Test1.cs
@@ -146,4 +146,66 @@ namespace TestBank
         }
 
     }
+
+    [TestClass]
+    public class TestKontoLimit
+    {
+        private KontoLimit konto;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            konto = new KontoLimit("Jan Kowalski", 100, 50);
+        }
+
+        [TestMethod]
+        public void Konstruktor_InicjalizujePola()
+        {
+            Assert.AreEqual("Jan Kowalski", konto.Nazwa);
+            Assert.AreEqual(150, konto.Bilans);
+            Assert.IsFalse(konto.Zablokowane);
+            Assert.AreEqual(50, konto.Limit);
+        }
+
+        [TestMethod]
+        public void Wyplata_WykorzystanieDebetu_BlokujeKonto()
+        {
+            konto.Wyplata(120);
+            Assert.AreEqual(0, konto.Bilans);
+            Assert.IsTrue(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void Wyplata_PrzekroczenieDebetu_ZwracaWyjatek()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => konto.Wyplata(200));
+        }
+
+        [TestMethod]
+        public void Wplata_PoWykorzystaniuDebetu_OdblokowujeKonto()
+        {
+            konto.Wyplata(120);
+            Assert.IsTrue(konto.Zablokowane);
+            konto.Wplata(20);
+            Assert.IsFalse(konto.Zablokowane);
+            Assert.AreEqual(50, konto.Bilans); 
+        }
+
+        [TestMethod]
+        public void ZmianaLimitu_ZmieniaDostepneSrodki()
+        {
+            konto.Limit = 100;
+            Assert.AreEqual(200, konto.Bilans);
+        }
+
+        [TestMethod]
+        public void Wyplata_CzescioweWykorzystanieDebetu()
+        {
+            konto.Wyplata(50);
+            Assert.AreEqual(100, konto.Bilans);
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+    }
+
 }
